34332c069ec20eb0c310cdd4b3cf14bd46f5e08b
kazuomi-nakajima <x15076.nakajima@gmail.com>
2020-12-08 19:40:03 +0900

    frist_testcode

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+ RSpec.describe User, type: :model do
+   describe 'ユーザー新規登録' do
+     it "nicknameが空だと登録できない" do
+      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000") #データを作成
+      user.valid? #保存されるものなのかどうかを確認
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
+    end
+     it "emailが空では登録できない" do
+      user = User.new(nickname: "abb", email: "", password: "00000000", password_confirmation: "00000000") #データを作成
+      user.valid?
+      expect(user.errors.full_messages).to include("Email can't be blank")
+     end
+   end
+ end
