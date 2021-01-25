e9a5938e66e5ccc1a6463b89da60a6a45cfac203
kazuomi-nakajima <x15076.nakajima@gmail.com>
2021-01-25 20:00:44 +0900

    検索機能実装

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,13 +1,7 @@
 class Tweet < ApplicationRecord
-  validates :text, presence: true
   belongs_to :user
   has_many :comments #commentテーブルとアソシエーション(複数形)
-  
-  def self.search(search)
-    if search != ""
-      Tweet.where('text LIKE(?)', "%#{search}%")
-    else
-      Tweet.all
-    end
-  end
+
+   # imageも空で投稿できないように追記
+  validates :text, :image, presence: true
 end
